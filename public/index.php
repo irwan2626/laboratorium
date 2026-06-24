a569e0daa67d9038a4acf05aee43f51d3f0833a3
Irwan <irwansyahsyah081@gmail.com>
2026-06-24 19:18:56 +0700

    Restore public files: index.php, .htaccess, favicon.ico, robots.txt (backup root index.php)

---
+++ public/index.php
@@ -0,0 +1,33 @@
+﻿<?php
+
+define('LARAVEL_START', microtime(true));
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides an auto-loader for the application.
+|
+*/
+require __DIR__.'/../vendor/autoload.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Bootstrap the application and handle the incoming request.
+|
+*/
+ = require_once __DIR__.'/../bootstrap/app.php';
+
+ = ->make(Illuminate\Contracts\Http\Kernel::class);
+
+ = ->handle(
+     = Illuminate\Http\Request::capture()
+);
+
+->send();
+
+->terminate(, );
